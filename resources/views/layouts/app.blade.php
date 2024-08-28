ae382ff4badf7c6f820d03dadaabeeab2dc0da24
Mekmun Sopheaktra <m-sopheaktra@beniten.com>
2024-08-28 14:19:46 +0700

    Update SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,16 @@
     <meta charset="utf-8">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="description" content="">
+    <meta name="description" content="Shop with Us">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:title" content="">
-    <meta property="og:type" content="">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
+    <meta property="og:title" content="{{ config('app.name', 'Laravel') }}">
+    <meta property="og:type" content="shop">
+    <meta property="og:url" content="https://shop.mekmunsopheaktra.com/">
+    <meta property="og:image" content="{{ asset('assets/imgs/logo/logo.png') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="shortcut icon" href="{{ asset('assets/imgs/logo/logo.png') }}" type="image/png">
     @livewireStyles
 </head>
 
